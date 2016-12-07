abc4c4b28682be176756ea329f9a61054a1b043e
Jeremy Haile <jhaile@gmail.com>
2016-12-06 22:03:24 -0500

    Try to parse Twilio message

--- santasinbox.rb
+++ santasinbox.rb
@@ -1,5 +1,24 @@
+require 'twilio-ruby'
 require 'sinatra'
 
-get '/' do
-  "Hey Lauren, welcome to Santa's Inbox"
+post '/message' do
+  puts params.inspect
+
+  response = nil
+  begin
+    if text = params[:message]
+      team_number, title = text.strip.split(" ", 2)
+      team_number = team_number.to_i
+    end
+
+    response = "Team #:#{team_number}\nTitle: #{title}\nURL: #{media_url}"
+
+  rescue => e
+    response = "There was a problem processing your message"
+  end
+
+  twiml = Twilio::TwiML::Response.new do |r|
+    r.Message(response)
+  end
+  twiml.text
 end
