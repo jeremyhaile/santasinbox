4b90eb4385046587b90d1d063dedc9953aa4f6e8
Jeremy Haile <jhaile@gmail.com>
2016-12-06 22:50:15 -0500

    Improved error handling

--- santasinbox.rb
+++ santasinbox.rb
@@ -10,24 +10,25 @@ post '/message' do
     if text = params["Body"]
       team_number, title = text.strip.split(" ", 2)
       team_number = team_number.to_i
-      title = title.strip
     end
 
+    raise "ERROR: Please provide a title for your image after your team number. For example: 5 Santa Claus"
+
     if params["NumMedia"] == "0"
-      message = "No image was attached to your text!"
+      message = "ERROR: No image was attached to your message!"
     else
       media_url = params["MediaUrl0"]
       message = "Team #:#{team_number}\nTitle: #{title}\nURL: #{media_url}"
 
       file = open(media_url)
       client = DropboxClient.new(ENV['DROPBOX_ACCESS_TOKEN'])
-      response = client.put_file("Team #{team_number}/#{title.gsub(/\s/, '_')}.jpg", file)
+      response = client.put_file("Team #{team_number}/#{title.strip.gsub(/\s/, '_')}.jpg", file)
       puts "Dropbox response: #{response}"
     end
 
   rescue => e
     puts "#{e.class}: #{e.message}\n#{e.backtrace.join("\n")}"
-    message = "There was a problem processing your message: #{e}"
+    message = "There was a problem processing your message: #{e.message}"
   end
 
   puts "Sending message: #{message}"
